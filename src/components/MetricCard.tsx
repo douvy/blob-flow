d9132e81b47fbe6a1091bd6ac1fb9fd1bc3a50cd
douvy <rjd5146@gmail.com>
2025-04-29 19:57:11 -0400

    Improve Live Metrics cards design

--- src/components/MetricCard.tsx
+++ src/components/MetricCard.tsx
@@ -11,27 +11,24 @@ interface MetricCardProps {
 
 export default function MetricCard({ title, value, trend = 'neutral', description }: MetricCardProps) {
   return (
-    <div className="bg-[#141519] rounded-lg p-4 shadow-md transition-all hover:shadow-lg border border-divider">
-      <div className="flex justify-between items-start mb-2">
-        <h3 className="text-secondaryText font-medium text-xs uppercase tracking-wider">{title}</h3>
-        {trend === 'up' && (
-          <span className="text-green-500">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z" clipRule="evenodd" />
-            </svg>
-          </span>
-        )}
-        {trend === 'down' && (
-          <span className="text-red-500">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M12 13a1 1 0 100 2h5a1 1 0 001-1v-5a1 1 0 10-2 0v2.586l-4.293-4.293a1 1 0 00-1.414 0L8 9.586l-4.293-4.293a1 1 0 00-1.414 1.414l5 5a1 1 0 001.414 0L11 9.414 14.586 13H12z" clipRule="evenodd" />
-            </svg>
-          </span>
-        )}
-      </div>
-      <div>
-        <p className="text-2xl font-semibold text-white">{value}</p>
-        {description && <p className="text-xs mt-1 text-secondaryText">{description}</p>}
+    <div className="relative h-full">
+      {/* Blue outer glow/border */}
+      <div className="absolute -inset-[2px] rounded-lg bg-[#0751c5]/25"></div>
+      
+      <div className="relative bg-[#141519] rounded-lg p-4 shadow-md transition-all hover:shadow-lg border border-divider h-full">
+        <div className="flex justify-between items-start mb-2">
+          <h3 className="text-secondaryText font-medium text-xs uppercase tracking-wider">{title}</h3>
+          {trend === 'up' && (
+            <i className="fa-regular fa-arrow-up-right text-green-400" aria-hidden="true"></i>
+          )}
+          {trend === 'down' && (
+            <i className="fa-regular fa-arrow-down-right text-red-400" aria-hidden="true"></i>
+          )}
+        </div>
+        <div>
+          <p className="text-2xl font-windsor-bold text-white">{value}</p>
+          {description && <p className="text-xs mt-1 text-secondaryText">{description}</p>}
+        </div>
       </div>
     </div>
   );
